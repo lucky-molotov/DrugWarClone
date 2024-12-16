d387814dbb8c18ca3504cf9e03043e101b94a513
lucky-molotov <166308508+lucky-molotov@users.noreply.github.com>
2024-12-16 17:24:15 +0000

    `Refactored game_loop.py: removed create_character and generate_landlord methods, replaced with direct calls to character_MGMT and character_MGMT.npc`

--- game_loop.py
+++ game_loop.py
@@ -3,19 +3,9 @@ import time,character_MGMT, message_events,hud,random
 
 
 class newgame():
-    #function to create a new character
-    def create_character():
-        player_character = character_MGMT.PlayerCharacter.create_character()
-        time.sleep(2)
-        
-        return player_character
-    
-     #function to create a new landlord   
-    def generate_landlord():
-        loanshark = character_MGMT.npc.create_npc("loan shark")
-        return loanshark
     #introduction
     def start_standard_game(character,loanshark):
+        
         debt=3000
         character.money = random.randint(500,3000)
         character.day = 30
@@ -24,10 +14,13 @@ class newgame():
         message_events.intro_messages(character,loanshark)
         hud.player_hud(character,character.day,character.money,debt,str(character.inventory))
         hud.actions_hud()
-        hud.inventory_hud()
+        hud.inventory_hud
     
-    character = create_character()
-    loanshark = generate_landlord()
+    character = character_MGMT()
+    loanshark = generate_loanshark()
     start_standard_game(character,loanshark)
 
-newgame()
+
+if __name__ == "__main__":
+    newgame()
+
